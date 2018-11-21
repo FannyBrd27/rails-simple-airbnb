8b4fa04e2335ac5a53fd33616e463056d9f0b453
Fanny Brouard <bfbrouard@gmail.com>
2018-11-21 19:07:59 +0100

    airbnb in progress

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,11 +1,33 @@
 class FlatsController < ApplicationController
-  before_action :set_flat, only: [:show]
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
 
   def index
     @flats = Flat.all
   end
 
   def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = flat.create(params[:flat_params])
+    redirect_to flat_path(@flat)
+  end
+
+  def edit
+  end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
   end
 
   private
